d77168245ebefe4f934cdefc382386e8ee972fcf
Romário Diogo <romariodiogo2021@gmail.com>
2022-07-13 03:48:44 -0300

    Desafio01

---
+++ desafios/bt.js
@@ -0,0 +1,9 @@
+function primeiro(){
+    window.alert("você apertou o Botão numero 1")
+}
+function segundo(){
+    window.alert("Você apertou o Botão numero 2")
+}
+function terceiro(){
+    window.alert("Você apertou o Botão numero 3")
+}
