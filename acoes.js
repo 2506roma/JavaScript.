1178972fa0369dba7bc382b0091fd29e6acf0b4f
Romário Diogo <romariodiogo2021@gmail.com>
2022-07-13 02:44:07 -0300

    js-externo

---
+++ acoes.js
@@ -0,0 +1,14 @@
+let res = window.document.getElementById('resposta')
+
+function acao01 (){
+    res.innerHTML+= `<p>Você apertou o botão 01<p>`
+}
+function acao02(){
+    res.innerHTML+= `<p>Você apertou o botão 02<p>`
+}
+function acao03(){
+    res.innerHTML+=`<p>Você apertou o botão 03</p>`
+}
+function acao04(){
+    res.innerHTML+=`<p>Você apertou o botão 04</p>`
+}
